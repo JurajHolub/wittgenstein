d459d16b458b7ddca2e037cc165d02b3fc5ba161
Juraj Holub <xholub40@stud.fit.vutbr.cz>
2022-04-05 13:37:36 +0200

    DoS attack util.

---
+++ simulator-server/protocols/src/main/java/net/consensys/wittgenstein/protocols/utils/DosAttackUtil.java
@@ -0,0 +1,42 @@
+package net.consensys.wittgenstein.protocols.utils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Utility helps to choose nodes for DoS attack.
+ */
+public class DosAttackUtil {
+
+    /**
+     * Use this in case of protocol with leader schedule for epoch.
+     * @return list of size numberOfNodesUnderAttack with most frequent leaders
+     */
+    public List<Integer> bestLeadersToAttackWithSchedule(List<Integer> leaderSchedule, int numberOfNodesUnderAttack) {
+        Map<Integer, Long> histogram = leaderSchedule.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        LinkedHashMap<Integer, Long> reverseSortedMap = new LinkedHashMap<>();
+        histogram.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+
+        List<Integer> mostFrequentLeaders = new ArrayList<>(reverseSortedMap.keySet()).stream()
+                .limit(numberOfNodesUnderAttack)
+                .collect(Collectors.toList());
+
+        return mostFrequentLeaders;
+    }
+
+    /**
+     * Use this in case of protocol with unpredictable VRF leader selection.
+     * @return list of size numberOfNodesUnderAttack with the richest nodes
+     */
+    public List<Integer> bestLeadersToAttackWithVDF(List<Integer> stakeDistribution, int numberOfNodesUnderAttack) {
+        Map<Integer, Long> stake = new HashMap<>();
+        for (int node = 0; node < stakeDistribution.size(); node++) {
+            stake.put(node, (long) stakeDistribution.get(node));
+        }
+        LinkedHashMap<Integer, Long> sorted = SortMapDescending.sort(stake);
+        return sorted.keySet().stream().limit(numberOfNodesUnderAttack).collect(Collectors.toList());
+    }
+}
